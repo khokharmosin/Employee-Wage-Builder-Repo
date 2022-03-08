2f31e4a8147e21175f74f5f83f8a73e3fa9cd7cf
khokharmosin <khokharmosin7806@gmail.com>
2022-03-08 10:36:10 +0530

    MODIFIED: Employee Part Time Full Time Wage Calculation

--- src/com/infogalaxy/institute/EmployeeWageBuilder.java
+++ src/com/infogalaxy/institute/EmployeeWageBuilder.java
@@ -4,20 +4,30 @@ public class EmployeeWageBuilder {
     public static void main(String[] args) {
         //Display Massage Welcome Employee Wage program
         System.out.println("Welcome To Employee Wage Computation Progarm..");
+        //UC-3 Checking Employee Part Time or Full Time
+        final int PART_TIME_HR = 4;
+        final int FULL_TIME_HR = 8;
         //UC-2 Calculate Daily Wage
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
-        int total = 0;
+        int total_Wage = 0;
         Random random = new Random();
         int empAttendance = random.nextInt()%2;
         //UC-1 Employee Attendance
         System.out.println(empAttendance);
         if(empAttendance == 0){
             System.out.println("Employee is Absent.");
-        }else{
+        }else {
             System.out.println("Employee is Present.");
-            total = WAGE_PER_HR*FULL_DAY_HR;
+            int emptype = random.nextInt() % 2;
+            System.out.println(emptype);
+            if (emptype == 0) {
+                System.out.println("Employee Is part Time.");
+                total_Wage = PART_TIME_HR * WAGE_PER_HR;
+            } else {
+                System.out.println("Employee Is Full Time.");
+                total_Wage = FULL_TIME_HR * WAGE_PER_HR;
+            }
         }
-        System.out.println("Employee Total Wage="+total);
+        System.out.println("Employee Total Wage="+total_Wage);
     }
 }
